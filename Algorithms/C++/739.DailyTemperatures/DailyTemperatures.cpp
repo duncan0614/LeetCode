aa5558e2009d6e5aa1c97584dc35c470557ef670
Duncan <s100092727@gmail.com>
2025-03-26 17:22:44 -0700

    Update new script
    853.CarFleet
    739.DailyTemperatures

---
+++ Algorithms/C++/739.DailyTemperatures/DailyTemperatures.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> ret(temperatures.size(), 0);
+        stack<pair<int, int>> stack;
+        
+        for (int i = 0; i < temperatures.size(); i++){
+            int t = temperatures[i];
+            while (!stack.empty() && t > stack.top().first){
+                auto pair = stack.top();
+                stack.pop();
+                ret[pair.second] = i - pair.second;
+            }
+
+            stack.push({t, i});
+        }
+
+        return ret;
+    }
+};
