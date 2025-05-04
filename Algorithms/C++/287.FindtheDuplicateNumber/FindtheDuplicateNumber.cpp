994466a928577184bf5c1184e10e3f54085a2084
Duncan <s100092727@gmail.com>
2025-05-04 00:10:33 -0700

    Add new script
    100. Same Tree
    104. Maximum Depth Of Binary Tree
    110. Balanced Binary Tree
    146. LRU Cache
    226. Invert Binary Tree
    235. Lowest Common Ancestor Of A Binary Search Tree
    287. Find the Duplicate Number
    543. Diameter Of Binary Tree
    572. Subtree Of Another Tree

---
+++ Algorithms/C++/287.FindtheDuplicateNumber/FindtheDuplicateNumber.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0, fast = 0;
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast)
+                break;
+        }
+
+        int slowMatch = 0;
+        while (true) {
+            slowMatch = nums[slowMatch];
+            slow = nums[slow];
+            if (slowMatch == slow)
+                return slow;
+        }
+    }
+};
+    
